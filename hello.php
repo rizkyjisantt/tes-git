5756f4a4d10a4ecde649afb89aa74540c34e702e
rizkyluxszerr <jinkzprolimit@gmail.com>
2022-03-11 21:44:49 +0700

    commit pertama mase

---
+++ hello.php
@@ -0,0 +1,5 @@
+<?php //disini termasuk php langsung tanpa html. Fungsi Echo sendiri untuk menampilkan teks ke layar 
+   echo "Nama: Mochammad Rizki Aji Santoso";
+   echo "<br/>";
+   echo "Nim: 200411100086";
+?>

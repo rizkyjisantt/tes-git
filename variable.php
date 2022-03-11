f1172b3ca82c4a81bcb0cc87b99a3c246e01d30c
rizkyluxszerr <jinkzprolimit@gmail.com>
2022-03-11 21:59:44 +0700

    commit kedua mase

---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php
+    echo "Nama: Mochammad Rizki Aji Santoso";
+    echo "<br/>";
+    echo "Nim: 200411100086";
+    $a = 5; //membuat variabel pertama
+    $b = 10; //membuat variabel kedua
+    $c = 20; //membuat variabel ketiga
+
+    $hasil1 = $b + $c - $a; //menambah variabel baru untuk melihat hasil dari operasi 3 variabel pertama 
+    $hasil2 = $a * $b + $c;
+    $hasil3 = $a * $c / $b;
+
+    echo"<br/>";
+    echo"Hasil Operasi Matematika yang Pertama adalah $hasil1 <br>"; //menampilkan hasil operasi matematika ke layar
+    echo"Hasil Operasi Matematika yang Kedua adalah $hasil2 <br>";
+    echo"Hasil Operasi Matematika yang Ketiga adalah $hasil3 <br>";
+?>
